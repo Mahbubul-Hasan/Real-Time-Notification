97b1d2bc4770fe348a562ffe8958d9b711639a8a
Mahbubul Hasan <mahbubul161@gmail.com>
2020-04-30 14:43:33 +0600

    Design done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,27 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">Create Post</div>
+                
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
-
-                    You are logged in!
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Post</span>
+                            </div>
+                            <textarea class="form-control" name="post" aria-label="With textarea"></textarea>
+                        </div>
+                        @error('post')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                        <input class="form-control btn btn-primary mt-3" type="submit" value="Post">
+                    </form>
                 </div>
             </div>
         </div>
