08cf1d0d52eea731138ae5da2180e6ddc25976f5
Mahbubul Hasan <mahbubul161@gmail.com>
2020-05-14 15:48:48 +0600

    Real time notification done

--- app/Notifications/NotifyAdminNotificaton.php
+++ app/Notifications/NotifyAdminNotificaton.php
@@ -38,26 +38,12 @@ class NotifyAdminNotificaton extends Notification
     }
 
     /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
+     * Get the database representation of the notification.
      *
      * @param  mixed  $notifiable
      * @return array
      */
-    public function toArray($notifiable)
+    public function toDatabase($notifiable)
     {
         return [
             "user" => $this->user,
